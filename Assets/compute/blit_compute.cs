7e895d4f03b5dc17e48db5bb9442b596c0304071
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-11-05 08:32:12 -0300

    adding template for compute shader

--- Assets/compute/blit_compute.cs
+++ Assets/compute/blit_compute.cs
@@ -6,18 +6,15 @@ public class BlitCompute : MonoBehaviour
     public Material mat;
     private RenderTexture renderTexture;
     private ComputeBuffer AgentDataBuffer;
-    public int numAgents = 1024;
-    public float agentRadius = 5.0f;
     private float time = 0.0f;
 
     void Start()
     {
         time = Time.time;
         InitializeRenderTexture();
-        InitializeComputeBuffer();
         DispatchComputeShader();
         mat.mainTexture = renderTexture;
-        
+
     }
 
     void Update()
@@ -32,51 +29,23 @@ public class BlitCompute : MonoBehaviour
         renderTexture.Create();
     }
 
-    void InitializeComputeBuffer()
-    {
-        AgentDataBuffer = new ComputeBuffer(numAgents, sizeof(float) * 4);
-        Vector4[] AgentData = new Vector4[numAgents];
-
-        for (int i = 0; i < numAgents; i++)
-        {   
-            float posX = Random.Range(5.0f, Screen.width-5.0f);
-            float posY = Random.Range(0, Screen.height-1.0f);
-            // float posX = Screen.width/2;
-            // float posY = Screen.height/2;
-            float speedX = Random.Range(-1f, 1f);
-            float speedY = Random.Range(-1f, 1f);
-
-            AgentData[i] = new Vector4(posX, posY, speedX, speedY);
-        }
-
-        AgentDataBuffer.SetData(AgentData);
-    }
 
     void DispatchComputeShader()
     {
         float dt = Time.time - time;
         time = Time.time;
         int kernelHandle = computeShader.FindKernel("CSMain");
-        
+
         computeShader.SetFloat("_dt", dt);
-        computeShader.SetFloat("_agentRadius", agentRadius);
         computeShader.SetTexture(kernelHandle, "Result", renderTexture);
-        computeShader.SetBuffer(kernelHandle, "AgentData", AgentDataBuffer);
         computeShader.SetInt("_width", renderTexture.width);
         computeShader.SetInt("_height", renderTexture.height);
-        computeShader.SetFloat("_time",Time.time);
+        computeShader.SetFloat("_time", Time.time);
 
-        int threadGroupsX = Mathf.CeilToInt(numAgents / 512.0f);
-        int threadGroupsY = Mathf.CeilToInt(renderTexture.height / 1.0f);
+        int threadGroupsX = Mathf.CeilToInt(renderTexture.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(renderTexture.height / 8.0f);
 
         computeShader.Dispatch(kernelHandle, threadGroupsX, threadGroupsY, 1);
-
-        int ImageProcessKernel = computeShader.FindKernel("ImageProcess");
-        int threadGroupsX2 = Mathf.CeilToInt(renderTexture.width / 8.0f);
-        int threadGroupsY2 = Mathf.CeilToInt(renderTexture.height / 8.0f);
-
-        computeShader.SetTexture(ImageProcessKernel, "Result", renderTexture);
-        computeShader.Dispatch(ImageProcessKernel, threadGroupsX2, threadGroupsY2, 1);
 
     }
 
@@ -84,7 +53,5 @@ public class BlitCompute : MonoBehaviour
     {
         if (renderTexture != null)
             renderTexture.Release();
-        if (AgentDataBuffer != null)
-            AgentDataBuffer.Release();
     }
 }
